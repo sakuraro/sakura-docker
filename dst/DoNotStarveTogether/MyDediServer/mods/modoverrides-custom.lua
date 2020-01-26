bcdf96f3b09d97407b82c469dda9c7df7d8d9903
sakura@sakura-pc <tomeetyouismyfortune@gmail.com>
2020-01-26 15:27:22 +0800

    init dst/

---
+++ dst/DoNotStarveTogether/MyDediServer/mods/modoverrides-custom.lua
@@ -0,0 +1,20 @@
+return {
+  -- Display food values
+  ["workshop-347079953"] = { enabled = true },
+  -- DST Storm Cellar
+  ["workshop-382177939"] = { enabled = true },
+  -- Extended Indicators
+  ["workshop-358749986"] = { enabled = true },
+  -- Extra Equip Slots
+  ["workshop-375850593"] = { enabled = true },
+  -- Fast Travel
+  ["workshop-458587300"] = { enabled = true },
+  -- Global Positions
+  ["workshop-378160973"] = { enabled = true },
+  -- Health Info
+  ["workshop-375859599"] = { enabled = true },
+  -- Map Discovery Sharing
+  ["workshop-462469447"] = { enabled = true },
+  -- Wormhole Marks
+  ["workshop-362175979"] = { enabled = true }
+}
